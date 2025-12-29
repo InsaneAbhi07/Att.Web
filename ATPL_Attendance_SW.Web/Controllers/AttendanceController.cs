3d5904765f4a25001d7252c77ea479bbe60ddc35
insaneAbhi07 <BelikeAbhi07@gmail.com>
2025-12-29 11:32:59 +0530

    AttCOn

---
+++ ATPL_Attendance_SW.Web/Controllers/AttendanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATPL_Attendance_SW.Web.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
